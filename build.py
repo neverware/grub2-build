c417c87a8d4b15b58693bddcd50fcc960896d098
Nicholas Bishop <nicholasbishop@gmail.com>
2020-08-02 15:24:51 -0400

    Update sources

--- build.py
+++ build.py
@@ -14,9 +14,15 @@ def main():
     container_dir = os.path.join(repo_dir, 'container')
 
     image = 'grub-build'
-    # TODO
-    grub_branch = 'bishop-grub-fedora33'
-    gnulib_revision = 'd271f868a8df9bbec29049d01e056481b7a1a263'
+    # TODO: might want to pin this to a particular commit and leave
+    # the branch name in a comment
+    grub_branch = 'neverware-fedora33'
+    # This revision is newer than what fedora-33 appears to be
+    # pointing to (d271f868a8 in a comment in the bootstrap.conf
+    # file). We need at least b7e213c072 to pull in a build fix. There
+    # are some newer fixes on top of that (in the fixes branch) but
+    # they introduce new compilation errors.
+    gnulib_revision = 'b7e213c072670ca78ebcdcc4853ece9c2452ee82'
 
     # yapf: disable
     run('docker', 'build',
